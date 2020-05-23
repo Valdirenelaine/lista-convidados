061f257683a3ca467bde018b48a579c9bbe968cd
Valdirene Elaine <valdirene.elaine@gmail.com>
2020-05-23 14:21:55 -0400

    ajustando o css com o boostrap

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function adicionar() {
     if (email == "") {
         alert("Preencha o email do Convidado")
     } else {
-        document.getElementById("imagem").src = "img/logotipo.png"
         nomesConvidados.push({ nome, idade, email })
         atualizarLista()
 
@@ -24,15 +23,22 @@ function adicionar() {
 
 function atualizarLista() {
     let lista = document.getElementById("divLista")
+    let tabela = document.getElementById("tabela")
     lista.innerHTML = ""
 
     for (let i = 0; i < nomesConvidados.length; i++) {
-
-        lista.innerHTML = lista.innerHTML + `<div>
-                           <span> Nome: ${nomesConvidados[i].nome}<br>  
-                           Idade: ${nomesConvidados[i].idade} anos <br>
-                           Email: ${nomesConvidados[i].email}</span>     
-                           </div>`
+        tabela.style.visibility = "visible"
+        lista.innerHTML = lista.innerHTML + `<tr>
+                           <td> ${i}</td>  
+                           <td> ${nomesConvidados[i].nome}</td>  
+                           <td> ${nomesConvidados[i].idade} </td>
+                           <td> ${nomesConvidados[i].email}</td>    
+                           <td> 
+                             <button onclick="remover(${i})" class="btn btn-link">
+                                <img src="img/trash.svg" width ="30px" alt="icone lixeira">
+                             </button> 
+                           </td>    
+                           </tr>`
     }
 
     lista.scrollTop = lista.scrollHeight - lista.clientHeight;
@@ -40,8 +46,15 @@ function atualizarLista() {
 
 }
 
+function remover(i) {
+    let tabela = document.getElementById("tabela")
+    nomesConvidados.splice(i, 1)
+    tabela.style.visibility = "hidden"
+    atualizarLista()
+
+}
+
 function limpar() {
-    document.getElementById("imagem").src = "img/logotipo1.png"
     let lista = document.getElementById("divLista")
     lista.innerHTML = ""
     nomesConvidados = []
