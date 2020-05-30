a8fb0522aa4e2a19fca18d149e5628ec5325f75d
Valdirene Elaine <valdirene.elaine@gmail.com>
2020-05-30 12:26:17 -0400

    ajustando as funções para excluir e alterar os registros.

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
 let nomesConvidados = []
+let indiceEdicao = -1
+let erros = []
 
-function adicionar() {
+function salvar() {
     let nome = document.getElementById("nomeConvidado").value
     let idade = document.getElementById("idadeConvidado").value
     let email = document.getElementById("emailConvidado").value
+    let valido = true
 
+    if (!nome) {
+        erros.push("O mome é obrigatório")
+        valido = false
+    }
+    if (!idade) {
+        erros.push("A idade é obrigatória")
+        valido = false
+    }
+    if (!email) {
+        erros.push("O email é obrigatório")
+        valido = false
+    }
+    if (!valido) {
+        exibirErros()
+        return
+    }
 
-    if (nome == "") {
-        alert("Preencha o nome do Convidado")
-    } else
-    if (idade == "") {
-        alert("Preencha a idade do Convidado")
-    } else
-    if (email == "") {
-        alert("Preencha o email do Convidado")
+    if (indiceEdicao > -1) {
+        nomesConvidados[indiceEdicao] = { nome, idade, email }
+
     } else {
         nomesConvidados.push({ nome, idade, email })
-        atualizarLista()
 
     }
+    atualizarLista()
+    limparFormulario()
+
+
+}
+
+function exibirErros() {
+    let secaoErros = document.getElementById("erros")
+    secaoErros.innerHTML = ""
+    erros.forEach(e => {
+
+        secaoErros.innerHTML = secaoErros.innerHTML += `
+                <div class="col col-7 alert alert-danger alert-dismissible fade show" role="alert">
+                    ${e}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                `
+    })
+    erros = []
+}
+
+function limparFormulario() {
+    indiceEdicao = -1
+    document.getElementById("nomeConvidado").value = ""
+    document.getElementById("idadeConvidado").value = ""
+    document.getElementById("emailConvidado").value = ""
+
 }
 
 function atualizarLista() {
@@ -34,6 +76,9 @@ function atualizarLista() {
                            <td> ${nomesConvidados[i].idade} </td>
                            <td> ${nomesConvidados[i].email}</td>    
                            <td> 
+                           <button onclick="editar(${i})" class="btn btn-link">
+                           <img src="img/pencil.svg" width ="30px" alt="icone editar">
+                          </button> 
                              <button onclick="remover(${i})" class="btn btn-link">
                                 <img src="img/trash.svg" width ="30px" alt="icone lixeira">
                              </button> 
@@ -54,11 +99,19 @@ function remover(i) {
 
 }
 
+function editar(i) {
+    indiceEdicao = i
+    document.getElementById("nomeConvidado").value = nomesConvidados[i].nome
+    document.getElementById("idadeConvidado").value = nomesConvidados[i].idade
+    document.getElementById("emailConvidado").value = nomesConvidados[i].email
+
+}
+
 function limpar() {
     let lista = document.getElementById("divLista")
     let tabela = document.getElementById("tabela")
     tabela.style.visibility = "hidden"
     lista.innerHTML = ""
     nomesConvidados = []
-
+    limparFormulario()
 }
